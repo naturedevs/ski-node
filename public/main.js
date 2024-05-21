bfc2c0df3e86ea2bc916eaa03bce98e724a5ba17
naturedevs <naturesky0411@gmail.com>
2024-05-21 11:34:37 +0200

    1

--- public/main.js
+++ public/main.js
@@ -1513,8 +1513,8 @@ $(function () {
         for(event of events) {
             $('#live-events').append($('<tr>'));
             tr = $('#live-events tr:last');
-            tr.append($('<td>').addClass("col-5 left").html("&nbsp"));
-            tr.append($('<td>').addClass("col-5 left").html("&nbsp"));
+            tr.append($('<td>').addClass("col-4 left").html("&nbsp"));
+            tr.append($('<td>').addClass("col-6 left").html("&nbsp"));
             tr.append($('<td>').addClass("col-2 center").html("&nbsp"));
 
             tr.children("td:nth-child(1)").html('<img src="./giphy.gif" style="width:80px"/>'+event.info.title);
